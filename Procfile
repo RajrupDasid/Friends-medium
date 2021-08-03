a7942b10b6352b5e2c8265c2e877f38512d21f42
RajrupDasid <rajrupdasid@outlook.com>
2021-08-03 15:30:25 +0530

    removing all heroku configurations

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn smedia.wsgi --log-file -
--- smedia/settings.py
+++ smedia/settings.py
@@ -164,5 +164,3 @@ MEDIA_ROOT=os.path.join(BASE_DIR,' media_root')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
